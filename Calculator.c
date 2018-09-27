91aa1ef27cb557fccaaaa02599b6804b602e1e2f
Thomas Atzlesberger <t.atzlesberger@gmx.at>
2018-09-27 11:43:05 +0200

    Add, Sub, Multi

--- Calculator.c
+++ Calculator.c
@@ -13,7 +13,7 @@ int number;
     printf("Wählen sie eine Option\n");
     printf("Add(1)\n");
     printf("Subtract(2)\n");
-    printf("Multiply(2)\n");
+    printf("Multiply(3)\n");
     printf("Devide(4)\n");
     printf("Stop Programm (-1)\n");
 
@@ -48,11 +48,22 @@ int number;
 
       differenc = minuent - subtrahent;
 
-      printf("Erbegbniss: %d", differenc );
+      printf("Erbegbniss: %d\n", differenc );
     }
     else if(number == 3)
     {
+        int faktorOne;
+        int faktorTwo;
+        int product;
 
+        printf("1. Produkt: ");
+        scanf("%d", &faktorOne );
+        printf("2.Produkt: ");
+        scanf("%d", &faktorTwo);
+
+        product = faktorOne*faktorTwo;
+
+        printf("Erbegbniss: %d\n", product );
     }
     else if(number == 4)
 
